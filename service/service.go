dfdbf2ceff9090ead0a14e009c4d72af3ffdf0cd
edmond fagbuyi <edmondfagbuyi@gmail.com>
2023-07-05 18:34:09 +0100

    filter handler

--- service/service.go
+++ service/service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"errors"
-	"log"
 	"strconv"
 
 	"github.com/durotimicodes/xanda_task_R3_D3/cmd/database"
@@ -41,41 +40,22 @@ func (db *MySQLDb) GetAll() ([]models.Spaceship, error) {
 }
 
 func (db *MySQLDb) FilterAllByName(name string) ([]models.Spaceship, error) {
-	var spaceships, filteredShips []models.Spaceship
+	var spaceships []models.Spaceship
 
-	database.DB.Find(&spaceships)
-
-	for _, spaceship := range spaceships {
-		if spaceship.Name == name {
-			filteredShips = append(filteredShips, spaceship)
-
-		}
+	resp := database.DB.Where("name = ?", name).Find(&spaceships)
+	if resp.Error != nil {
+		return nil, resp.Error
 	}
 
-	err := database.DB.Preload("Name").Find(&filteredShips).Error
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-
-	return filteredShips, nil
+	return spaceships, nil
 }
 
 func (db *MySQLDb) FilterAllByClass(class string) ([]models.Spaceship, error) {
 	var spaceships []models.Spaceship
 
-	database.DB.Find(&spaceships)
-
-	for _, spaceship := range spaceships {
-		if spaceship.Class == class {
-			spaceships = append(spaceships, spaceship)
-		}
-	}
-
-	err := database.DB.Preload("Class").Find(&spaceships).Error
-	if err != nil {
-		log.Println(err)
-		return nil, err
+	resp := database.DB.Where("class = ?", class).Find(&spaceships)
+	if resp.Error != nil {
+		return nil, resp.Error
 	}
 
 	return spaceships, nil
@@ -83,24 +63,14 @@ func (db *MySQLDb) FilterAllByClass(class string) ([]models.Spaceship, error) {
 
 func (db *MySQLDb) FilterAllByStatus(status string) ([]models.Spaceship, error) {
 
-	var spaceships, filteredShips []models.Spaceship
+	var spaceships []models.Spaceship
 
-	database.DB.Find(&spaceships)
-
-	for _, spaceship := range spaceships {
-		if spaceship.Status == status {
-			filteredShips = append(filteredShips, spaceship)
-
-		}
+	resp := database.DB.Where("status = ?", status).Find(&spaceships)
+	if resp.Error != nil {
+		return nil, resp.Error
 	}
 
-	err := database.DB.Preload("Status").Find(&filteredShips).Error
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-
-	return filteredShips, nil
+	return spaceships, nil
 }
 
 func (db *MySQLDb) GetSingleSpaceship(Id int) (*models.Spaceship, error) {
@@ -160,23 +130,23 @@ func (db *MySQLDb) UpdateSpaceship(id int, name, class, status string, crew int,
 		return nil, errors.New("spaceship not valid")
 	}
 
-	err := db.DB.Model(&spaceship).Where("id = ?", id).
+	resp := db.DB.Model(&spaceship).Where("id = ?", id).
 		Update("name", name).
 		Update("class", class).
 		Update("armaments", armaments).
 		Update("crew", crew).
 		Update("value", value).
-		Update("status", status).Error
+		Update("status", status)
 
-	if err != nil {
-		return nil, err
+	if resp.Error != nil {
+		return nil, resp.Error
 	}
 
 	for i := range armaments {
 		armaments[i].SpaceshipID = spaceship.ID
 	}
 
-	resp := db.DB.Model(&armaments).Where("id = ?", strconv.Itoa(id))
+	resp = db.DB.Model(&armaments).Where("id = ?", strconv.Itoa(id))
 
 	return map[string]bool{"success": true}, resp.Error
 
@@ -197,6 +167,10 @@ func (db *MySQLDb) DeleteSpaceship(Id int) (map[string]bool, error) {
 
 	return map[string]bool{"success": true}, nil
 }
+
+// -------- Repository End --------
+
+
 
 // -------- Services Begin --------
 func GetAllSpaceShips() ([]models.Spaceship, error) {
@@ -224,17 +198,15 @@ func GetSpaceship(id int) (*models.Spaceship, error) {
 	return r.GetSingleSpaceship(id)
 }
 
-func UpdateSpaceship(id int,name, class, status string, crew int, value float32, armaments []models.Armament ) (map[string]bool, error) {
+func UpdateSpaceship(id int, name, class, status string, crew int, value float32, armaments []models.Armament) (map[string]bool, error) {
 	r := &MySQLDb{}
 	return r.UpdateSpaceship(id, name, class, status, crew, value, armaments)
 }
 
-// func CreateNewSpaceship(name, class, status string, crew int, value float32) map[string]bool {
-// 	r := &MySQLDb{}
-// 	return r.CreateSpaceship(name, class, status, crew, value)
-// }
 
 func DeleteSpaceshipByID(id int) (map[string]bool, error) {
 	r := &MySQLDb{}
 	return r.DeleteSpaceship(id)
 }
+
+// -------- Services End --------
